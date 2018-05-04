e8db11a4193aa6106bc2bcea2be560c533864aa8
Walter Junior <walter.junior@meupag.com.br>
2018-05-04 10:17:18 -0300

    commit first

---
+++ app-AccessContactsLocal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-AccessContactsLocal
+//
+//  Created by Walter Junior on 04/05/2018.
+//  Copyright © 2018 Walter Junior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
