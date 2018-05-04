c4c67d0f274a417e7cc8591ef54195e2e9e0764c
Walter Junior <walter.junior@meupag.com.br>
2018-05-04 11:27:32 -0300

    Fetch contatcs in store

---
+++ app-AccessContactsLocal/ContactStruct.swift
@@ -0,0 +1,15 @@
+//
+//  ContactStruct.swift
+//  app-AccessContactsLocal
+//
+//  Created by Walter Junior on 04/05/2018.
+//  Copyright © 2018 Walter Junior. All rights reserved.
+//
+
+import Foundation
+
+struct ContactStruct {
+    let givenName: String
+    let familyName: String
+    let number: String
+}
--- app-AccessContactsLocal/ViewController.swift
+++ app-AccessContactsLocal/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import Contacts
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - outlet / variable
+    @IBOutlet weak var tableView: UITableView!
+    var contactStore = CNContactStore()
+    var contacts = [ContactStruct]()
+    
+    
+    // MARK: - view manipulation
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        requestContactsStore()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    // MARK: - functions
+    private func requestContactsStore() {
+        contactStore.requestAccess(for: .contacts) { (success, error) in
+            if success {
+                print("Authorization Successfull...")
+            }
+        }
+        
+        fetchContacts()
+    }
+    
+    func fetchContacts() {
+        
+        let key = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey] as [CNKeyDescriptor]
+        
+        let request = CNContactFetchRequest(keysToFetch: key)
+        
+         try! contactStore.enumerateContacts(with: request) { (contact, stoppingPointer) in
+            let name = contact.givenName
+            let familyName = contact.familyName
+            let number = contact.phoneNumbers.first?.value.stringValue
+            
+            let contactToAppend = ContactStruct(givenName: name, familyName: familyName, number: number!)
+            
+            self.contacts.append(contactToAppend)
+        }
+        
+        tableView.reloadData()
+        
+    }
 
 }
 
